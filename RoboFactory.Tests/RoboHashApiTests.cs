444f3056f84f5009f63b0a42cc26ba8427ddb5c4
Wayu <m.me.dev@outlook.com>
2024-11-07 09:12:05 +0700

    Add basic test cases for RoboHashApi

---
+++ RoboFactory.Tests/RoboHashApiTests.cs
@@ -0,0 +1,53 @@
+﻿using System.Drawing;
+
+namespace RoboFactory.Tests
+{
+    public class RoboHashApiTests
+    {
+        private Bitmap? imageResult;
+
+        [SetUp]
+        public void Setup()
+        {
+            // Arrange
+            var hash = new Random().Next(0, 100).ToString();
+            // Act
+            imageResult = RoboHashApi.GenerateAsync(hash).Result;
+        }
+
+        [TearDown]
+        public void Teardown()
+        {
+            // Dispose
+            imageResult?.Dispose();
+        }
+
+        [Test]
+        public void Generate_ShouldNotReturnNull()
+        {
+            // Assert
+            Assert.That(imageResult, Is.Not.Null);
+        }
+
+        [Test]
+        public void Generate_ShouldReturnBitmao()
+        {
+            // Assert
+            Assert.That(imageResult, Is.InstanceOf<Bitmap>());
+        }
+
+        [Test]
+        public void Generate_ShouldReturnDefaultSizeImage()
+        {
+            // Arrange
+            var defaultWidth = 300;
+            var defaultHeight = 300;
+
+            // Assert
+            Assert.Multiple(() => {
+                Assert.That(imageResult?.Width, Is.EqualTo(defaultWidth));
+                Assert.That(imageResult?.Height, Is.EqualTo(defaultHeight));
+            });
+        }
+    }
+}
