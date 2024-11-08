db7a9c87cf45c30825f4396090b72ea10e57661c
Wayu <m.me.dev@outlook.com>
2024-11-08 09:29:50 +0700

    Add initial test setup for Robot class

---
+++ RoboFactory.Tests/RobotTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoboFactory.Tests
+{
+    public class RobotTests
+    {
+        private Robot? robot;
+
+        [SetUp]
+        public void Setup()
+        {
+            // Arrange
+            robot = Robot.GenerateAsync().Result;
+        }
+
+        [TearDown] public void Teardown()
+        {
+            
+        }
+
+        [Test]
+        public void Generate_ShouldNotReturnNull()
+        {
+            // Assert
+            Assert.That(robot, Is.Not.Null);
+        }
+    }
+}
